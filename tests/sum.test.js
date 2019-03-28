b440c97ac54c6250167087098aa7cc3117157994
Brian Kim <brianjunewookim@gmail.com>
2019-03-28 11:19:51 -0700

    Make all jest tests pass

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -4,12 +4,12 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 1 + 2 to equal 5', () => {
-  expect(sum(1, 2)).toBe(5);
+test('adds 3 + 2 to equal 5', () => {
+  expect(sum(3, 2)).toBe(5);
 });
 
-test('adds 1 + 2 to equal 9', () => {
-  expect(sum(1, 2)).toBe(9);
+test('adds 7 + 2 to equal 9', () => {
+  expect(sum(7, 2)).toBe(9);
 });
 
 test('adds 4 + 2 to equal 6', () => {
