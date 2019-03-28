4efb66dd59e0883daeda228e7eefb07db18e68af
Brian Kim <brianjunewookim@gmail.com>
2019-03-28 11:12:40 -0700

    Add fourth test for jest

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -11,3 +11,7 @@ test('adds 1 + 2 to equal 5', () => {
 test('adds 1 + 2 to equal 9', () => {
   expect(sum(1, 2)).toBe(9);
 });
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(4, 2)).toBe(4);
+});
