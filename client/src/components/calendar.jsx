42dcdfdc41d6e30c817f73b9d5aa0a88cd781d7f
Brian Kim <brianjunewookim@gmail.com>
2019-03-29 19:30:49 -0700

    Create basic react components and reder it

---
+++ client/src/components/calendar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Calendar extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h1>send help</h1>
+    )
+  }
+}
+
+export default Calendar;
---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calendar from './components/calendar.jsx';
+
+ReactDOM.render(<Calendar />, document.getElementById('app'));
