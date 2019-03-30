2754a6084c9675fd4feaa88ee642e7131f448347
Brian Kim <brianjunewookim@gmail.com>
2019-03-29 19:51:59 -0700

    Update jest config to work with enzyme

--- tests/calendar.test.js
+++ tests/calendar.test.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Calendar from '../client/src/components/calendar';
 import { shallow } from 'enzyme';
 
-describe('Going to test for something', () => {
+it('Going to test for something', () => {
   const wrapper = shallow(<Calendar />);
-  test('Says send help', () => {
-    expect(wrapper.text()).toEqual('send help');
-  });
+  expect(wrapper.text()).toEqual('send help');
 });
