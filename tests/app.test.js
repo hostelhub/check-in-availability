a4ef26290c73b59f7a0b854c6edf63d247166c3d
Brian Kim <brianjunewookim@gmail.com>
2019-04-04 21:32:17 -0700

    Create basic tests for components

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import App from '../client/src/components/App';
+import Calendar from '../client/src/components/Calendar'
 import { shallow } from 'enzyme';
 
-it('Going to test for something', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.text()).toEqual('send help');
-});
+describe('Calendar component application', () => {
+  it('Tests for properly rendered application.', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+  it('Tests for properly rendering Calendar component.', () => {
+    const wrapper = shallow(<Calendar />);
+    expect(wrapper.exists()).toBe(true);
+  })
+})
+
