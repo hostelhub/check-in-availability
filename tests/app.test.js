c7f1a074675ac620cd82ab104484ed39c7aeed1e
Brian Kim <brianjunewookim@gmail.com>
2019-04-10 10:37:28 -0700

    Fix tests for altered component names

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import App from '../client/src/components/App';
-import Calendar from '../client/src/components/Calendar'
+import Calendar from '../client/src/components/Calendar';
+import CalendarDates from '../client/src/components/CalendarDates'
+import Weeks from '../client.src.components.Weeks'
 import { shallow } from 'enzyme';
 
 describe('Calendar component application', () => {
   it('Tests for properly rendered application.', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Calendar />);
     expect(wrapper.exists()).toBe(true);
   });
   
-  it('Tests for properly rendering Calendar component.', () => {
-    const wrapper = shallow(<Calendar />);
+  it('Tests for properly rendering CalendarDates component.', () => {
+    const wrapper = shallow(<CalendarDates />);
     expect(wrapper.exists()).toBe(true);
-  })
+  });
+
+  it('Tests for properly rendering weeks component.', () => {
+    const wrapper = shallow(<Weeks />);
+    expect(wrapper.exists()).toBe(true);
+  });
 })
