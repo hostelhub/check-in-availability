b8ead76f6a751d83160142fc3872980e3038fef9
Brian Kim <brianjunewookim@gmail.com>
2019-04-11 11:43:50 -0700

    Change div id for proxy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Calendar from './components/Calendar.jsx';
 
-ReactDOM.render(<Calendar />, document.getElementById('app'));
+ReactDOM.render(<Calendar />, document.getElementById('calendar'));
